4bb4661d7e54b8124b736d796eabea691a2c77f7
Cole <colemooney5@gmail.com>
2021-12-27 15:37:17 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css';
 import Header from "./components/header"
 
@@ -14,13 +14,14 @@ class App extends Component {
   return (
     <Router>
       <Header />
-      <div className="container">
+      <div className="App">
         <h2> MERN-Stack Todo App </h2>
 
-
+      <Routes>
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
+      </Routes>
       </div>
     </Router>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,52 @@
-import React from "react";
-import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function Header(){
+
+const useStyles = makeStyles(theme => ({
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+const Header = () =>{
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
     return (
-      <AppBar position='static'>
-         <Toolbar>
-             <Typography>React Navbar Example</Typography>
-         </Toolbar>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Title
+          </Typography>
+          
+        </Toolbar>
+        
       </AppBar>
-
     );
-}
+  };
 export default Header;
