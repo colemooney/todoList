6f3ea1942af37340819a20ed88748a2c17505c10
Cole <colemooney5@gmail.com>
2021-12-27 15:25:09 -0800

    new front page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Header from "./components/header"
 
-function App() {
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+
+class App extends Component {
+  render(){
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <div className="container">
+        <h2> MERN-Stack Todo App </h2>
+
+
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </div>
+    </Router>
   );
 }
-
+}
 export default App;
